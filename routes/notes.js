2d003a15e7bea74d1d93db08ea3add7f62645506
NileshPatel83 <hselin_83@yahoo.com>
2023-03-09 06:20:11 +1000

    Delete route added to delete note with specified id.

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,5 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 const databaseFilePath = './db/db.json';
 
@@ -30,12 +30,55 @@ notes.post('/', (req, res) => {
     //Adds the new note to current database file.
     readAndAppend(newNote, databaseFilePath);
 
-    //Sends success status.
+    //Sends success response with new note.
     res.status(200).json(newNote);
 
-    //Sends bad request status.
+    //Sends bad request response.
     } else {
         res.status(400).json('Error in adding note');
+    }
+});
+
+//Delete route to delete note with specified id.
+notes.delete('/:id', (req, res) => {
+
+    //Checks whether the note id is provided or not. 
+    if(req.params.id){
+
+        //Gets note id from route parameters.
+        const noteId = req.params.id;
+
+        //Reads existing note data from database file.
+        readFromFile(databaseFilePath).then((data) => {
+            
+            //Converts the data in jason format.
+            const extNotes = JSON.parse(data);
+
+            //Tries to find the note with specified id from existing note array.
+            const extNote = extNotes.filter(note => note.id === noteId)[0];
+
+            //Checks if note with specified id exist or not.
+            if(extNote){
+
+                //Creates a new note array by removing the note with specified id.
+                const newNotes = extNotes.filter(note => note.id !== noteId);
+
+                //Overwrites the database file with new notes array (with removed note with speficied id)
+                writeToFile(databaseFilePath, newNotes);
+
+                //Sends success response with new notes array.
+                res.status(200).json(newNotes);
+
+            //Sends bad request response if note id doesn't match existing notes.
+            } else{
+                res.status(400).json(`Note ID doesn't match.`);
+            }
+
+        });
+    
+    //Sends bad request response if note id is not provided.
+    } else {
+        res.status(400).json('Note ID not provided.');
     }
 });
 
